7c5bcd39a1dd6569a5fd8c4dd029244eb62f2448
Naveen-GDTC <167775781+Naveen-GDTC@users.noreply.github.com>
2024-08-16 09:45:19 +0530

    script.py

--- script.py
+++ script.py
@@ -1,7 +1,6 @@
 import requests
 from bs4 import BeautifulSoup as bs
 import pandas as pd
-import csv
 import psycopg2
 from sqlalchemy import create_engine
 
@@ -21,11 +20,6 @@ for row in tdata.find_all('tr'):
     table_data.append(row_data)
 
 
-with open("table_data.csv", 'w', newline='') as file:
-    writer = csv.writer(file)
-    writer.writerows(table_data)
-
-
 df_table = pd.DataFrame(table_data)
 df_table.iloc[0,0] = 'Section'
 df_table.columns = df_table.iloc[0]
@@ -33,7 +27,7 @@ df_table = df_table[1:]
 # df_table = df_table.set_index('')
 
 
-db_host = "192.168.29.101"#"192.168.1.223"
+db_host = "192.168.1.223" #"192.168.29.101"
 db_name = "reliance"
 db_user = "docker"
 db_password = "docker"
@@ -43,4 +37,4 @@ engine = create_engine(f'postgresql+psycopg2://{db_user}:{db_password}@{db_host}
 
 df_table.to_sql('profit_loss_data', engine, if_exists='replace', index=False)
 
-print("Data loaded successfully into PostgreSQL database")
+print("Data loaded to PostgreSQL")
