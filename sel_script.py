93b58abdf3e09a6ff9917cdf3f0e02d522c194e2
Naveen-GDTC <167775781+Naveen-GDTC@users.noreply.github.com>
2024-08-23 16:06:15 +0530

    Update sel_script.py

--- sel_script.py
+++ sel_script.py
@@ -8,7 +8,6 @@ from selenium.webdriver.chrome.service import Service
 import time
 import os
 
-# Set up Chrome options
 chrome_options = Options()
 chrome_options.add_argument("--headless")  # Run in headless mode
 chrome_options.add_argument("--disable-gpu")
@@ -16,7 +15,6 @@ chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--window-size=1920,1080")
 
-# Set download directory if needed
 current_dir = os.getcwd()
 prefs = {
     "download.default_directory": current_dir,
@@ -26,15 +24,51 @@ prefs = {
 }
 chrome_options.add_experimental_option("prefs", prefs)
 
-# Specify the path to ChromeDriver
 service = Service('/usr/local/bin/chromedriver')
 
-# Initialize the WebDriver
 driver = webdriver.Chrome(service=service, options=chrome_options)
 
-# Example usage
+def login(username, password):
+    login_button = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, '//*[contains(concat( " ", @class, " " ), concat( " ", "account", " " ))]'))
+    )
+    login_button.click()
+
+    email_input = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, '//*[(@id = "id_username")]'))
+    )
+    password_input = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, '//*[(@id = "id_password")]'))
+    )
+
+    email_input.send_keys(username)
+    password_input.send_keys(password)
+
+    second_login_button = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, '//*[contains(concat( " ", @class, " " ), concat( " ", "icon-user", " " ))]'))
+    )
+    second_login_button.click()
+
+
 try:
-    driver.get("https://www.example.com")
-    print("Page title is:", driver.title)
+    driver.get("https://www.screener.in/company/RELIANCE/consolidated/")
+
+    export = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, '//*[contains(concat( " ", @class, " " ), concat( " ", "icon-download", " " ))]'))
+    )
+    export.click()
+
+    login("firstscreener123@gmail.com", "Asdf!234")
+
+    export = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, '//*[contains(concat( " ", @class, " " ), concat( " ", "icon-download", " " ))]'))
+    )
+    export.click()
+
+    download_dir = current_dir
+    print("Files in download directory before wait:", os.listdir(download_dir))
+
 finally:
     driver.quit()
+
+
