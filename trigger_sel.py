e782833169a90693c3ba41202fa2b680bf9fe288
Naveen-GDTC <167775781+Naveen-GDTC@users.noreply.github.com>
2024-08-27 18:02:33 +0530

    Update trigger_sel.py

--- trigger_sel.py
+++ trigger_sel.py
@@ -1,10 +1,35 @@
 import psycopg2
+import pandas as pd 
+import os 
+
+stock_code = os.environ.get('STOCK_CODE')
+current_dir = os.getcwd()
+
+download_dir = current_dir
+print("Files in download directory before wait:", os.listdir(download_dir))
+
+files = os.listdir(download_dir)
+excel_file = [i for i in files if i.endswith('.xlsx')]
+print(excel_file)
+
+df = pd.read_excel(excel_file[0], sheet_name='Data Sheet', header=14)
+df = df.head(16)
+
+df = df.rename(columns={'PROFIT & LOSS':''}).T
+df.columns = df.iloc[0]
+df = df.iloc[1:]
+df = df.reset_index(drop = True)
+for i in df.iloc[:,1:].columns:
+    df[i] = df[i].astype('int')
+df['stock'] =  stock_code
+
+print(df)
 
 db_params = {
     'dbname': 'reliance',
     'user': 'docker',
     'password': 'docker',
-    'host': "192.168.1.188",#'192.168.1.223',#"192.168.29.101",#
+    'host': "192.168.1.188",
     'port': 5432
 }
 
@@ -27,7 +52,8 @@ create_table_query= '''CREATE TABLE IF NOT EXISTS profit_loss_data_sel (
     profit_before_tax INT ,          
     tax INT ,                        
     net_profit INT ,                 
-    dividend_amount INT              
+    dividend_amount INT,
+    stock varchar(20)
 );
 
 CREATE TABLE IF NOT EXISTS profit_loss_data_sel_changes (
@@ -48,7 +74,8 @@ CREATE TABLE IF NOT EXISTS profit_loss_data_sel_changes (
     profit_before_tax INT,
     tax INT,
     net_profit INT,
-    dividend_amount INT
+    dividend_amount INT,
+    stock varchar(20)
 );
 
 CREATE OR REPLACE FUNCTION profit_loss_data_sel_changes() RETURNS TRIGGER AS $$
@@ -57,34 +84,34 @@ BEGIN
         INSERT INTO profit_loss_data_sel_changes (
             operation, report_date, sales, raw_material_cost, change_in_inventory, power_and_fuel,
             other_mfr_exp, employee_cost, selling_and_admin, other_expenses, other_income,
-            depreciation, interest, profit_before_tax, tax, net_profit, dividend_amount
+            depreciation, interest, profit_before_tax, tax, net_profit, dividend_amount,stock
         ) VALUES (
             'INSERT', NEW.report_date, NEW.sales, NEW.raw_material_cost, NEW.change_in_inventory,
             NEW.power_and_fuel, NEW.other_mfr_exp, NEW.employee_cost, NEW.selling_and_admin,
             NEW.other_expenses, NEW.other_income, NEW.depreciation, NEW.interest,
-            NEW.profit_before_tax, NEW.tax, NEW.net_profit, NEW.dividend_amount
+            NEW.profit_before_tax, NEW.tax, NEW.net_profit, NEW.dividend_amount, NEW.stock
         );
     ELSIF TG_OP = 'UPDATE' THEN
         INSERT INTO profit_loss_data_sel_changes (
             operation, report_date, sales, raw_material_cost, change_in_inventory, power_and_fuel,
             other_mfr_exp, employee_cost, selling_and_admin, other_expenses, other_income,
-            depreciation, interest, profit_before_tax, tax, net_profit, dividend_amount
+            depreciation, interest, profit_before_tax, tax, net_profit, dividend_amount,stock
         ) VALUES (
             'UPDATE', NEW.report_date, NEW.sales, NEW.raw_material_cost, NEW.change_in_inventory,
             NEW.power_and_fuel, NEW.other_mfr_exp, NEW.employee_cost, NEW.selling_and_admin,
             NEW.other_expenses, NEW.other_income, NEW.depreciation, NEW.interest,
-            NEW.profit_before_tax, NEW.tax, NEW.net_profit, NEW.dividend_amount
+            NEW.profit_before_tax, NEW.tax, NEW.net_profit, NEW.dividend_amount, NEW.stock
         );
     ELSIF TG_OP = 'DELETE' THEN
         INSERT INTO profit_loss_data_sel_changes (
             operation, report_date, sales, raw_material_cost, change_in_inventory, power_and_fuel,
             other_mfr_exp, employee_cost, selling_and_admin, other_expenses, other_income,
-            depreciation, interest, profit_before_tax, tax, net_profit, dividend_amount
+            depreciation, interest, profit_before_tax, tax, net_profit, dividend_amount,stock
         ) VALUES (
             'DELETE', OLD.report_date, OLD.sales, OLD.raw_material_cost, OLD.change_in_inventory,
             OLD.power_and_fuel, OLD.other_mfr_exp, OLD.employee_cost, OLD.selling_and_admin,
             OLD.other_expenses, OLD.other_income, OLD.depreciation, OLD.interest,
-            OLD.profit_before_tax, OLD.tax, OLD.net_profit, OLD.dividend_amount
+            OLD.profit_before_tax, OLD.tax, OLD.net_profit, OLD.dividend_amount,OLD.stock
         );
     END IF;
     RETURN NEW;
@@ -104,8 +131,8 @@ print("table created")
 insert_query = '''INSERT INTO profit_loss_data_sel (
     report_date, sales, raw_material_cost, change_in_inventory, power_and_fuel, other_mfr_exp,              
     employee_cost, selling_and_admin, other_expenses, other_income, depreciation,              
-    interest, profit_before_tax, tax, net_profit, dividend_amount              
-) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);
+    interest, profit_before_tax, tax, net_profit, dividend_amount,stock              
+) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);
 '''
 for x, row in df.iterrows():
     cur.execute(insert_query, tuple(row))
